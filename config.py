2dd88f29577c3d15305b3e185317225beb559589
mrbean04a <128343665+mrbean04a@users.noreply.github.com>
2023-03-24 05:44:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ from os import environ
 API_ID = int(environ.get("API_ID", "22923523"))
 API_HASH = environ.get("API_HASH", "d52c7824d0e66903a0724b800a16ce2c")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6175608481:AAEvbfpHWEESKojdPd7CU8wGZYztIeOrq1s")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+ADMIN = int(environ.get("ADMIN", "0"))          
+CAPTION = environ.get("CAPTION", "@trendyanime")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
